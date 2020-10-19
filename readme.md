ab6814204ae79087dedbbefb11c5fc647043020d
Sheraz Ishaq <sheraz_1991@yahoo.com>
2020-10-19 23:16:19 +0500

    highlight function completed

---
+++ readme.md
@@ -0,0 +1,8 @@
+# CSV Column Highlighter Function
+
+
+- function depend on `xlsxwriter` library.
+
+- highlight code details must be in `code_highlighted_abstract2` column
+
+- function will take data in `abstract2` column and generate highlighted data in new column with name `highlighted_abstract2`
--- script.py
+++ script.py
@@ -5,87 +5,54 @@ import csv
 import pdb
 
 
-class HighPointAxis:
+def highlight_csv_column(csv_file_path):
+	xlsx_file_path = csv_file_path.replace(".csv", ".xlsx")
+	workbook = xlsxwriter.Workbook(xlsx_file_path)
+	worksheet = workbook.add_worksheet()
 
-	def __init__(self, position=None, offset=None, color=None):
-		self.color = color		
-		self.offset = offset
-		self.position = position
+	headers = []
+	column_number = 0
+	highlighted_abstract_column_name = "highlighted_abstract2"
+
+	for index, row in enumerate(csv.DictReader(open(csv_file_path))):
+		# write headers
+		if index == 0:
+			headers = list(row.keys()) + [highlighted_abstract_column_name]
+			worksheet.write_row(0, 0, headers)
+
+			# get the column number to highlight
+			# use it later while writting
+			column_number = next((__index for __index, _r in enumerate(headers) if _r == highlighted_abstract_column_name), None)
+
+		worksheet.write_row(index + 1, 0, list(row.values()) + [row["abstract2"]])
+
+		relative_offset = 0
+		cell = row["abstract2"]
+		formatted_collection = [cell]
+
+		highlight_code = eval(row["code_highlighted_abstract2"])
+		for h_point in highlight_code:
+
+			position = h_point['position'] - relative_offset
+			cell = formatted_collection.pop()
+
+			formatted_collection += [
+				cell[:position],
+				workbook.add_format({'color': h_point['color']}),
+				cell[position:position + h_point['offset']],
+				cell[position + h_point['offset']:]
+			]
+
+			relative_offset = h_point['position'] + h_point['offset']
+
+		formatted_collection = list(filter(None, formatted_collection))
+		worksheet.write_rich_string("{0}{1}".format(chr(column_number + 65), index + 2), *formatted_collection)
+
+	workbook.close()
+	return xlsx_file_path
 
 
-class HighPoint:
 
-	def __init__(self, row=None, column=None, column_name=None):
-		self.row = row
-		self.column = column
-		self.column_name = column_name
-
-		self.axis_collection = []
-
-	def add_axis(self, position=None, offset=None, color=None):
-		axis = HighPointAxis(position, offset, color)
-		self.axis_collection.append(axis)
-
-
-
-csv_file_path = "example.csv"
-
-xlsx_file_path = csv_file_path.replace(".csv", ".xlsx")
-workbook = xlsxwriter.Workbook(xlsx_file_path)
-worksheet = workbook.add_worksheet()
-
-data = []
-headers = []
-
-for index, row in enumerate(csv.DictReader(open(csv_file_path))):
-	# write headers
-	if index == 0:
-		headers = list(row.keys())
-		worksheet.write_row(0, 0, headers)
-
-	worksheet.write_row(index + 1, 0, row.values())
-	data.append(row)
-
-
-# creating dummy data
-highlight_points = []
-for index in range(0, len(data)):
-
-	colors = ["red", "yellow", "green"]
-
-	column_name = "E Mail"
-	column_number = next((__index for __index, _r in enumerate(headers) if _r == column_name), None)
-
-	h_point = HighPoint(index, column_number, column_name)
-	h_point.add_axis(0, 2, "green")
-	h_point.add_axis(3, 2, "yellow")
-	h_point.add_axis(6, 2, "red")
-
-	highlight_points.append(h_point)
-
-# real working
-for h_point in highlight_points:
-	print("Row: {0}".format(h_point.row))
-
-	relative_offset = 0
-	cell = data[h_point.row][h_point.column_name]
-	formatted_collection = [cell]
-
-	for axis in h_point.axis_collection:
-		position = axis.position - relative_offset
-		cell = formatted_collection.pop()
-
-		formatted_collection += [
-			cell[:position],
-			workbook.add_format({'color': axis.color}),
-			cell[position:position + axis.offset],
-			cell[position + axis.offset:]
-		]
-
-		relative_offset = axis.position + axis.offset
-
-	formatted_collection = list(filter(None, formatted_collection))
-	worksheet.write_rich_string("{0}{1}".format(chr(h_point.column + 65), h_point.row + 2), *formatted_collection)
-
-workbook.close()
-
+csv_file_path = "simple.csv"
+csv_file_path = "test_highlight.csv"
+highlight_csv_column(csv_file_path)
